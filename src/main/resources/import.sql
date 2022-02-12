760cd4bea1c498febb4f34b28f3e095ffaf33711
jordan <16jloudis@gmail.com>
2022-02-11 21:39:47 -0500

    JPA creates database and populates data with user and pass credentials

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+# INSERT INTO `practice`. `users` (username, password, role)VALUES ('user1','pass1', 'ROLE_USER');
+
+insert into practice.users (user_id, enabled, password, role, username ) values (1, 1, '$2a$12$DOij6Zn2l/X5QK1UP1HizeKYZgjNqgWjvIaRRdaA2KmfLNRtsOYne','ROLE_USER', 'user');
+
